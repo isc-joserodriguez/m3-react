7f7eb7d24bfad169d9305026986097f0e1fbf2f7
José Rodriguez <isc.joserodriguez@gmail.com>
2021-12-18 11:00:59 -0700

    Posts Init

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Posts = () => {
+const Posts = ({ postList }) => {
     return (
         <div>
-            Posts List
+            {postList.map((el) => (
+                <>
+                    <br />
+                    <div>{JSON.stringify(el)}</div>
+                </>
+            ))}
         </div>
     )
 }
