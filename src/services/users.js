c9d633c04afe0896945cc16936d17b1052024a86
José Rodriguez <isc.joserodriguez@gmail.com>
2022-01-08 12:51:10 -0700

    Validation

--- src/services/users.js
+++ src/services/users.js
@@ -3,12 +3,21 @@ import axios from 'axios';
 const URL_ROOT = `${process.env.REACT_APP_API}/user`;
 
 export const signupService = async (user) => {
-  const resp = await axios.post(`${URL_ROOT}/signup`, user);
-  return resp.data
+  try {
+    const resp = await axios.post(`${URL_ROOT}/signup`, user);
+    return resp.data
+  } catch (e) {
+    console.log('1',e.data.message)
+    console.log('2',e.message)
+  }
 }
 
 export const loginService = async (user) => {
-  console.log(user);
-  const resp = await axios.post(`${URL_ROOT}/login`, user);
-  return resp.data
+  try {
+    const resp = await axios.post(`${URL_ROOT}/login`, user);
+    return resp.data
+  } catch (e) {
+    console.log('1',e.data.message)
+    console.log('2',e.message)
+  }
 }
