72888de319f6392ff4f4a573609a404fca87fbfb
José Rodriguez <isc.joserodriguez@gmail.com>
2022-01-08 12:02:53 -0700

    Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import { loginService } from '../../services';
 
 const Login = () => {
-    const onSubmit = (e) => {
+    const { saveToken, user: token } = useContext(UserContext);
+
+    const onSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const dataObject = Object.fromEntries(formData);
-        console.log(dataObject);
+        const user = await loginService(dataObject);
+        saveToken(user.detail);
         e.target.reset();
     };
+    const redirect = <Navigate to='/' />
+
     return (
-        <form onSubmit={onSubmit} >
-            <input type="text" placeholder="Email" name="mail" />
-            <br /><br />
-            <input type="password" placeholder="Password" name="password" />
-            <br /><br />
-            <input type="submit" value="Login" />
-        </form>
+        token
+            ? redirect
+            : <form onSubmit={onSubmit} >
+                <input type="text" placeholder="Email" name="mail" />
+                <br /><br />
+                <input type="password" placeholder="Password" name="password" />
+                <br /><br />
+                <input type="submit" value="Login" />
+            </form>
     )
 }
 
