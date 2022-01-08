af2f4326522e3bfc6dda866a83fbc844eb726e16
José Rodriguez <isc.joserodriguez@gmail.com>
2022-01-08 11:58:53 -0700

    Token Authorization

--- src/services/categories.js
+++ src/services/categories.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const URL_ROOT = `${process.env.REACT_APP_API}/category`;
 
 export const getCategories = async () => {
-  const resp = await axios.get(`${URL_ROOT}/`);
+  const resp = await axios.get(`${URL_ROOT}/`, {
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('token')
+    }
+  });
   return resp.data
 }
